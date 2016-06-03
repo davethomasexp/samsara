6569f9cf1895a5c460fea3277867991879afbe76
davethomasexp <xiiimorti@gmail.com>
2016-06-03 10:52:42 -0400

    more db migrations for linking user_id to movies created, adds paperclip to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603140454) do
+ActiveRecord::Schema.define(version: 20160603142117) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title",        limit: 255
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160603140454) do
     t.string   "rating",       limit: 255
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "user_id",      limit: 4
   end
 
   create_table "users", force: :cascade do |t|
