9b9c913ac04d3aa8834c8b7831611e859673beb7
davethomasexp <xiiimorti@gmail.com>
2016-06-03 11:00:09 -0400

    images for movies

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603142117) do
+ActiveRecord::Schema.define(version: 20160603145840) do
 
   create_table "movies", force: :cascade do |t|
-    t.string   "title",        limit: 255
-    t.text     "description",  limit: 65535
-    t.string   "movie_length", limit: 255
-    t.string   "director",     limit: 255
-    t.string   "rating",       limit: 255
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "user_id",      limit: 4
+    t.string   "title",              limit: 255
+    t.text     "description",        limit: 65535
+    t.string   "movie_length",       limit: 255
+    t.string   "director",           limit: 255
+    t.string   "rating",             limit: 255
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "user_id",            limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,6 +41,10 @@ ActiveRecord::Schema.define(version: 20160603142117) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "image_file_name",        limit: 255
+    t.string   "image_content_type",     limit: 255
+    t.integer  "image_file_size",        limit: 4
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
