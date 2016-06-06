34d937796a9b7b1fd51da66d5c474ac23ce37923
davethomasexp <xiiimorti@gmail.com>
2016-06-06 17:35:40 -0400

    adds raty.js as well as updates ratings numbers to stars

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606202232) do
+ActiveRecord::Schema.define(version: 20160606203319) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title",              limit: 255
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160606202232) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "user_id",    limit: 4
+    t.integer  "movie_id",   limit: 4
   end
 
   create_table "users", force: :cascade do |t|
